eba85498eea930090b73222c41369711a5d2a104
Sam Griffith <samgriffith3@gmail.com>
2019-01-19 00:30:12 -0500

    New Commit, If Statements

---
+++ if-test/if_hostname.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+
+hostname = input('Your Text Here: ')
+host = hostname.lower()
+if host == 'mtg':
+	print('The hostname was found to be mtg')
+
+print("exiting the script")
+
